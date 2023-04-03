3aece2a4f18d3dae2d3a0556c773fe72985ada16
HaneenOqailan <haneenoqailan@gmail.com>
2023-04-03 14:26:01 +0300

    first commit

---
+++ Restaurant/Restaurant/RestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantApp.swift
+//  Restaurant
+//
+//  Created by haneen eqylan on 03/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
